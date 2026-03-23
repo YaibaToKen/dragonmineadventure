f277dba4932e418cae60067c6283d43d7c65f72b
Shokkoh <ezshokkoh@gmail.com>
2026-03-22 22:37:07 -0300

    fix: stack form mastery

--- src/main/java/com/dragonminez/Reference.java
+++ src/main/java/com/dragonminez/Reference.java
@@ -2,7 +2,7 @@ package com.dragonminez;
 
 public class Reference {
 	public static final String MOD_ID = "dragonminez";
-	public static final String VERSION = "2.0.3";
+	public static final String VERSION = "2.0.4-beta";
 	public static final String EMPTY = "%s:empty".formatted(MOD_ID);
 
 	private Reference() {
